a0dbe904fc1f5859635d88011156b89fb4fb6717
prateek-ml <prateekb1912@gmail.com>
2021-07-11 19:28:22 +0530

    Scrape names and prices for sofas from first page

--- spiders/pepperfry_spider.py
+++ spiders/pepperfry_spider.py
@@ -8,5 +8,14 @@ class PepperFry(scrapy.Spider):
 
         yield scrapy.Request(sofas_url, self.parse)
 
-    def parse(self):
-        pass
+    def parse(self, response):
+        sofa_divs = response.css('div.clip-crd-10x11')
+
+        for sd in sofa_divs:
+            sofa_name = sd.css('h2 a::text').get()
+            sofa_price = sd.css('span.clip-offr-price::text').get()[2:]
+
+            yield {
+                'Model': sofa_name,
+                'Price': sofa_price
+            }
